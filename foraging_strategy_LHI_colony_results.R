b724a4b6932c7efe6e2f075e0a7aed0e28e703c1
Mark Miller <mark.gr.miller@gmail.com>
2015-09-15 15:45:50 +1000

    cleaning up the sloppy loops and allowing implimentation of non-defined trip return loop

--- foraging_strategy_LHI_colony_results.R
+++ foraging_strategy_LHI_colony_results.R
@@ -40,43 +40,62 @@ fun_both<-function(x)
 
 # function for single adult feeds
 fun_single<-function(x) 
-  { 
+{ 
   LWO<-sample(c("B", "A"),1,)
-     if(LWO=="B"){RWO<-"A"}else{RWO<-"B"}
+  if(LWO=="B"){RWO<-"A"}else{RWO<-"B"}
   
   return(c(LWO, RWO))}
 
+### Use this to set specific dates to test temporal foraging strat changes
+
+D1="2015-02-18"
+D2="2015-03-01"  
+
+#nest_comp<-nest_comp[nest_comp$Date<"2015-03-02",] #just 2 weeks in Feb
+#nest_comp<-nest_comp[nest_comp$Date<"2015-03-19",] # allows 1 month from mid feb start (for long trips to get back)
+#nest_comp<-nest_comp[nest_comp$Date>"2015-03-10",] # allows 1 month from mid march to apr
 
 ## bootstrapping
 bs1000_tl<-data.frame(tLength=seq(1:25))
-for(h in 1:100)
+for(h in 1:1000)
   
 {
   out<-lapply(nest_comp[nest_comp$LW_corr=="D",]$diff, FUN=fun_single)  
   
-  nest_comp[nest_comp$LW_corr=="D",]$LW_assn<-unlist(out)[seq(1,287,2)]
-  nest_comp[nest_comp$LW_corr=="D",]$RW_assn<-unlist(out)[seq(2,288,2)]
-  
-
-  #nest_comp<-nest_comp[nest_comp$Date<"2015-03-02",] 
+  nest_comp[nest_comp$LW_corr=="D",]$LW_assn<-unlist(out)[seq(1,(length(unlist(out))-1),2)]
+  nest_comp[nest_comp$LW_corr=="D",]$RW_assn<-unlist(out)[seq(2,length(unlist(out)),2)]
   
   all_trips<-NULL
   for(i in unique(nest_comp$NestID))
   {
-    for(j in c("LW", "RW"))
+    for(j in c("LW", "RW")) # the below calc of backs allows trips that start within the time period but not end to be included
     {
-      backs<-NULL;dunnos<-NULL
+      backs<-NULL
       if(j=="LW")
-      {backs<-which(nest_comp[nest_comp$NestID==i,]$LW_assn=="B")
-       dunnos<-which(nest_comp[nest_comp$NestID==i,]$LW_assn=="D")}
+      {backs<-which(nest_comp[nest_comp$NestID==i& nest_comp$Date>=D1 & nest_comp$Date<=D2,]$LW_assn=="B")
+       
+       if(max(backs)<nrow(nest_comp[nest_comp$NestID==i& nest_comp$Date>=D1 & nest_comp$Date<=D2,]))
+       {backs<-c(backs, which(nest_comp[nest_comp$NestID==i& nest_comp$Date>=D1,]$LW_assn=="B")[length(backs)+1])
+        backs<-na.omit(backs)}
+      }
+      
       if(j=="RW")
-      {backs<-which(nest_comp[nest_comp$NestID==i,]$RW_assn=="B")
-       dunnos<-which(nest_comp[nest_comp$NestID==i,]$RW_assn=="D")}
+      {backs<-which(nest_comp[nest_comp$NestID==i& nest_comp$Date>=D1 & nest_comp$Date<=D2,]$RW_assn=="B")
+       
+       if(max(backs)<nrow(nest_comp[nest_comp$NestID==i& nest_comp$Date>=D1 & nest_comp$Date<=D2,]))
+       {backs<-c(backs, which(nest_comp[nest_comp$NestID==i& nest_comp$Date>=D1,]$RW_assn=="B")[length(backs)+1])
+        backs<-na.omit(backs)}
+      }
+      
+      #if(j=="LW")
+      #{backs<-which(nest_comp[nest_comp$NestID==i,]$LW_assn=="B")}
+      #if(j=="RW")
+      #{backs<-which(nest_comp[nest_comp$NestID==i,]$RW_assn=="B")}
+      
       
       trip_lz<-NULL  
       for(k in 2:length(backs))
       {
-        if(TRUE %in% (dunnos>backs[k-1] & dunnos<backs[k])){next}
         tl1<-backs[k]-backs[k-1]
         trip_lz<-c(trip_lz, tl1) 
       }
@@ -90,6 +109,7 @@ for(h in 1:100)
   trip_propz<-NULL
   for(i in unique(all_trips$BirdID2))
   {
+    if(is.na(sum(all_trips[all_trips$BirdID2==i,]$tLength))){next} # this next catches any NA birds for whatever reason and removes. corrects warning created in max(backs) above
     df1<-data.frame(BirdID2=i,table(all_trips[all_trips$BirdID2==i,]$tLength))
     df1$Var1<-as.numeric(as.character(df1$Var1))
     df1$TotTrip<-df1$Var1 * df1$Freq 
@@ -117,8 +137,8 @@ for(h in 1:100)
 bs1000_tl[bs1000_tl==0]<-NA
 
 bs_smry<-data.frame(tLength=seq(1:25))
-bs_smry$mean_prop<-apply(bs1000_tl[,2:101],1,mean, na.rm=T)
-bs_smry$sd_prop<-apply(bs1000_tl[,2:101],1,sd, na.rm=T)
+bs_smry$mean_prop<-apply(bs1000_tl[,2:1001],1,mean, na.rm=T)
+bs_smry$sd_prop<-apply(bs1000_tl[,2:1001],1,sd, na.rm=T)
 
 bs_smry<-na.omit(bs_smry)
 
@@ -129,6 +149,5 @@ pp+geom_bar(stat="identity", fill="dark grey", colour="black")+
   scale_x_continuous(breaks=seq(1:25))+
   theme_classic()+ylab("Mean time spent forgaing (prop)")+xlab("Duration of foraging trip (days)")
 
-ggsave("LHI_foraging_feb_apr_15_single_feed_assn.png")
-
+ggsave("LHI_foraging_mar_apr_15_single_feed_assn.png")
 
