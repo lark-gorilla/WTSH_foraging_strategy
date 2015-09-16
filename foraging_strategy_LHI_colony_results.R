98b89cb68234d5e155a5052bb8ffa671b25bad58
Mark Miller <mark.gr.miller@gmail.com>
2015-09-16 11:53:00 +1000

    Fixed key issues with proportion calculation (0 instead of NA) and added individual nest calculation functionality.

--- foraging_strategy_LHI_colony_results.R
+++ foraging_strategy_LHI_colony_results.R
@@ -1,3 +1,4 @@
+rm(list=ls())
 library(ggplot2)
 library(lubridate)
 
@@ -18,7 +19,7 @@ meal_mod_dat<-rbind(data.frame(Feeder="both", Meal_size =nest_comp[nest_comp$LW_
                     data.frame(Feeder="single", Meal_size =nest_comp[nest_comp$RW_corr=="B" & nest_comp$LW_corr=="A" & nest_comp$Chick=="Fed",]$diff))
 
 
-m1<-glm(Feeder~Meal_size, data=meal_hist_dat, family=binomial)
+m1<-glm(Feeder~Meal_size, data=meal_mod_dat, family=binomial)
 
 
 # function for single and potential both adult feeds
@@ -46,15 +47,19 @@ fun_single<-function(x)
   
   return(c(LWO, RWO))}
 
+#### !!! Always run function from here !!! to reset all data.frames !!!
+
 ### Parameters
 
 D1="2015-02-18"
-D2="2015-03-01" 
+D2="2015-04-11" 
 longallow=TRUE
 feed_fun=fun_single
 
 ## bootstrapping
 bs1000_tl<-data.frame(tLength=seq(1:25))
+bs1000_Ntl<-expand.grid(tLength=seq(1:25), NestID=unique(nest_comp$NestID))
+
 for(h in 1:1000)
   
 {
@@ -112,8 +117,28 @@ for(h in 1:1000)
       }
   }
   
-  all_trips$BirdID2<-paste(all_trips$NestID, all_trips$BirdID,sep="_")
+  # to calc hists per individual nests (pairs) %%$$
+  nest_propz<-NULL
+  tripPropN<-expand.grid(tLength=seq(1:25), NestID=unique(nest_comp$NestID), tProp=0)
+  for(i in unique(all_trips$NestID))
+  {
+    if(is.na(sum(all_trips[all_trips$NestID==i,]$tLength))){next} # this next catches any NA birds for whatever reason and removes. corrects warning created in max(backs) above
+    df1<-data.frame(NestID=i,table(all_trips[all_trips$NestID==i,]$tLength))
+    df1$Var1<-as.numeric(as.character(df1$Var1))
+    df1$TotTrip<-df1$Var1 * df1$Freq 
+    df1$TripProp<-df1$TotTrip/sum(df1$TotTrip)
+    nest_propz<-rbind(nest_propz, df1)
+    
+    for(j in unique(df1$Var1))
+      {
+      tripPropN[tripPropN$NestID==i & tripPropN$tLength==j,]$tProp<-df1[df1$Var1==j,]$TripProp
+      }
+    
+  }
+  # %%$$
   
+  # to calc hists per individual birds ~~**
+  all_trips$BirdID2<-paste(all_trips$NestID, all_trips$BirdID,sep="_")
   trip_propz<-NULL
   for(i in unique(all_trips$BirdID2))
   {
@@ -126,29 +151,33 @@ for(h in 1:1000)
     trip_propz<-rbind(trip_propz, df1)
   }
   
-  length(unique(trip_propz$BirdID2))
-  #shows how many birds are monitored
-  
+  #length(unique(trip_propz$BirdID2)) #shows how many birds are monitored
   tripPropB<-NULL
   for(i in 1:25)
   { 
     df1<-data.frame(tLength=i, tProp=sum(trip_propz[trip_propz$Var1==i,]$TripProp)/length(unique(trip_propz$BirdID2)))
     tripPropB<-rbind(tripPropB, df1)
   }
+  # ~~**
   
+  # compile birds bootstrap
   bs1000_tl<-cbind(bs1000_tl, tripPropB[,2] )
-  
   names(bs1000_tl)[names(bs1000_tl)=="tripPropB[, 2]"]<-paste("run", h, sep="_")
+
+  
+  # compile nests bootstrap
+  bs1000_Ntl<-cbind(bs1000_Ntl, tripPropN[,3] )
+  names(bs1000_Ntl)[names(bs1000_Ntl)=="tripPropN[, 3]"]<-paste("run", h, sep="_")
   print(h)
 }
 
-bs1000_tl[bs1000_tl==0]<-NA
+#plot all birds
 
 bs_smry<-data.frame(tLength=seq(1:25))
 bs_smry$mean_prop<-apply(bs1000_tl[,2:1001],1,mean, na.rm=T)
 bs_smry$sd_prop<-apply(bs1000_tl[,2:1001],1,sd, na.rm=T)
 
-bs_smry<-na.omit(bs_smry)
+bs_smry[bs_smry==0]<-NA #makes graph neater 
 
 limits <- aes(ymax = mean_prop + sd_prop, ymin=mean_prop - sd_prop)
 pp<-ggplot(bs_smry, aes(x=tLength, y=mean_prop))
@@ -158,3 +187,21 @@ pp+geom_bar(stat="identity", fill="dark grey", colour="black")+
   theme_classic()+ylab("Mean time spent forgaing (prop)")+xlab("Duration of foraging trip (days)")
 
 ggsave(paste("LHI15_foraging",D1, D2, "longallow", longallow, "feed_fun_SINGLE.png", sep="_")) #change feed_fun manually!
+
+#plot individual nests
+
+bs_Nsmry<-bs1000_Ntl[,1:2]
+bs_Nsmry$mean_prop<-apply(bs1000_Ntl[,3:1002],1,mean, na.rm=T)
+bs_Nsmry$sd_prop<-apply(bs1000_Ntl[,3:1002],1,sd, na.rm=T)
+
+bs_Nsmry[bs_Nsmry==0]<-NA #makes graph neater 
+
+limits <- aes(ymax = mean_prop + sd_prop, ymin=mean_prop - sd_prop)
+pp<-ggplot(bs_Nsmry, aes(x=tLength, y=mean_prop))
+pp+geom_bar(stat="identity", fill="dark grey", colour="black")+
+  geom_errorbar(limits, width=0.5)+
+  scale_x_continuous(breaks=seq(1,25,2))+
+  theme_classic()+ylab("Mean time spent forgaing (prop)")+xlab("Duration of foraging trip (days)")+facet_wrap(~NestID)
+
+ggsave(paste("LHI15_Nest_foraging",D1, D2, "longallow", longallow, "feed_fun_SINGLE.png", sep="_")) #change feed_fun manually!
+
