f037a70fba66ed1d1b5e8ab99aef53e17d93003c
lark-gorilla <mark.gr.miller@gmail.com>
2016-04-07 17:54:39 +1000

    correcting timezones!

--- foraging_strategy_LHI_tracking.R
+++ foraging_strategy_LHI_tracking.R
@@ -20,15 +20,9 @@ gps_dat<-rbind(gps_2014, gps_2015, gps_2016)
 
 head(gps_dat)
  
-gps_dat$DateTime<-as.POSIXlt(paste(gps_dat$Date, gps_dat$Time, tz="AEST"), format = "%Y/%m/%d %H:%M:%S")
-
-#!!!! FROM NOW ALL TZ WILL BE SET TO AEST, CHECK FUNCTION SETTINGS!!!
-
+gps_dat$DateTime<-as.POSIXlt(paste(gps_dat$Date, gps_dat$Time, tz="GMT"), format = "%Y/%m/%d %H:%M:%S")
+#!!!! FROM NOW ALL TZ WILL BE SET TO GMT (the R default as my system has Sys.timezone==NA), even though its not actually it makes life so much easier!!!
 gps_dat$TrackTime <- as.double(gps_dat$DateTime)
-
-gps_dat$day<-day(gps_dat$DateTime)
-gps_dat$month<-month(gps_dat$DateTime)
-gps_dat$year<-year(gps_dat$DateTime)
 
 plot(Latitude~Longitude, gps_dat, pch=16, cex=0.5, col=factor(year))
 map("worldHires", add=T,  col=5)
@@ -82,47 +76,103 @@ datout<-na.omit(datout)
   resamp<-rbind(resamp, resample_output)
   }
 
-write.csv(resamp, "LHI_gps_14_15_16.csv", quote=F, row.names=F)  
-  
-datout<-resamp
+#write.csv(resamp, "LHI_gps_14_15_16_interp.csv", quote=F, row.names=F)  
+#resamp<-read.csv("LHI_gps_14_15_16_interp.csv", h=T)  
 
-plot(Latitude~Longitude, datout, pch=16, cex=0.4, col=factor(TrackID))
-map("worldHires", add=T, col=3)
+#correct Date, Time and DateTime fields to reflect interpolation changes    
+resamp$Date<- format(as.Date(as.POSIXlt(resamp$TrackTime, origin="1970-01-01")), format="%Y/%m/%d")
+resamp$Time <- format((as.POSIXlt(resamp$TrackTime, origin="1970-01-01")), "%H:%M:%S")  
+resamp$DateTime<-as.POSIXlt(paste(resamp$Date, resamp$Time), format = "%Y/%m/%d %H:%M:%S")
 
-require(geosphere)
+library(lubridate)
+
+resamp$day<-day(resamp$DateTime)
+resamp$month<-month(resamp$DateTime)
+resamp$year<-year(resamp$DateTime)
+
+
+#kill unneccessary fields, as resample has made non-sensical duplicates
+resamp$Date_resamp<-NULL
+resamp$Time_resamp<-NULL
+resamp$Altitude<-NULL
+resamp$Speed<-NULL
+resamp$Course<-NULL
+resamp$Type<-NULL
+resamp$Distance<-NULL
+resamp$Essential<-NULL
+resamp$Vel<-NULL
+
 require(sp)
 require(rgdal)
-require(rgeos)
+require(maps)
+require(mapdata)
 require(maptools)
 
 ##Tripsplit
 
 signal_point<-data.frame(Latitude=-31.524732, Longitude=159.059787)
 
-
 #### Trip split ####
-source("~/grive/phd/scripts/MIBA_scripts_revised/TripSplit_revised.r")
+source("~/grive/phd/scripts/MIBA_scripts_revised/TripSplit_revised_MMLINUX.r")
 
-birds <-unique(datout$TrackID)
-datout$ID<-datout$TrackID
+birds <-unique(resamp$TrackID)
+resamp$ID<-resamp$TrackID
 
 
 for(i in birds)
 {
-  Temp <- datout[datout$TrackID==i,]
+  Temp <- resamp[resamp$TrackID==i,]
   
   Trip <- tripSplit(Track=Temp, Colony=signal_point, InnerBuff=1, ReturnBuff=5, Duration=0.5, plotit=T)
   
   if(which(birds==i) == 1) {Trips <- Trip} else
     Trips <- spRbind(Trips,Trip)
   
-  readline("")
+  #readline("")
 }
 
-write.csv(Trips@data, "LHI_14_15_gps_interp_tripsplit.csv", quote=F, row.names=F)
+write.csv(Trips@data, "LHI_gps_14_15_16_interp_tripsplit.csv", quote=F, row.names=F)
 
 plot(Trips)
 plot(Trips[Trips$trip_id==-1,], col=2, add=T)
+
+## Trip lengths from GPS data
+
+trip_lens<-NULL
+for(j in unique(Trips$trip_id))
+{
+  trippy<-Trips[Trips$trip_id==j,]
+  diff<-((trippy[nrow(trippy),]$TrackTime-trippy[1,]$TrackTime)/3600)/24
+  out<-data.frame(trip_id=j, trip_len=diff, st_dt=trippy[1,]$DateTime, 
+                  nd_dt=trippy[nrow(trippy),]$DateTime, st_dist=trippy[1,]$ColDist, 
+                  nd_dist=trippy[nrow(trippy),]$ColDist, return=unique(trippy$Returns))
+  trip_lens<-rbind(trip_lens, out)
+}
+
+write.csv(trip_lens, "trip_lengths_from_gps.csv", quote=F, row.names=F)
+# Add in the actual return dates of missing trips manually, then read back in
+t_len_corr<-read.csv("trip_lengths_from_gps_act_return.csv", h=T) 
+
+t_len_corr$DateTime<-as.POSIXlt(t_len_corr$st_dt, format = "%d/%m/%Y %H:%M")
+t_len_corr$st_tt <- as.double(t_len_corr$DateTime)
+
+
+t_len_corr$DateTime_nd<-as.POSIXlt(t_len_corr$Actual_return, format = "%d/%m/%Y %H:%M")
+t_len_corr$nd_tt <- as.double(t_len_corr$DateTime_nd)
+
+t_len_corr$trip_len2<-((t_len_corr$nd_tt-t_len_corr$st_tt)/3600)/24
+
+t_len_corr[which(is.na(t_len_corr$trip_len2)),]$trip_len2<-
+  t_len_corr[which(is.na(t_len_corr$trip_len2)),]$trip_len
+
+qplot(data=t_len_corr, x=trip_len2, geom="histogram")
+
+## plots for when birds are on colony/foraging to improve attendence data
+
+
+
+
+#plots
 
 library(ggplot2)
 
@@ -161,35 +211,6 @@ p2
 ggsave(filename="LHI_gps_trips2.png", scale=2)
 
 graphics.off()
-
-trip_lens<-NULL
-for(j in unique(Trips$trip_id))
-{
-  trippy<-Trips[Trips$trip_id==j,]
-  diff<-((trippy[nrow(trippy),]$TrackTime-trippy[1,]$TrackTime)/3600)/24
-  out<-data.frame(trip_id=j, trip_len=diff, st_dt=trippy[1,]$DateTime, 
-                  nd_dt=trippy[nrow(trippy),]$DateTime, st_dist=trippy[1,]$Distance, 
-                  nd_dist=trippy[nrow(trippy),]$Distance, return=unique(trippy$Returns))
-  trip_lens<-rbind(trip_lens, out)
-}
-
-write.csv(trip_lens, "trip_lengths_from_gps.csv", quote=F, row.names=F)
-
-t_len_corr<-read.csv("trip_lengths_from_gps.csv", h=T)
-
-t_len_corr$DateTime<-as.POSIXlt(t_len_corr$st_dt, format = "%d/%m/%Y %H:%M")
-t_len_corr$st_tt <- as.double(t_len_corr$DateTime)
-
-
-t_len_corr$DateTime_nd<-as.POSIXlt(t_len_corr$Actual_return, format = "%d/%m/%Y %H:%M")
-t_len_corr$nd_tt <- as.double(t_len_corr$DateTime_nd)
-
-t_len_corr$trip_len2<-((t_len_corr$nd_tt-t_len_corr$st_tt)/3600)/24
-
-t_len_corr[which(is.na(t_len_corr$trip_len2)),]$trip_len2<-
-  t_len_corr[which(is.na(t_len_corr$trip_len2)),]$trip_len
-
-qplot(data=t_len_corr, x=trip_len2, geom="histogram")
 
 
 
