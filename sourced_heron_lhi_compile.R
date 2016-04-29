eb0a5ee3d4dae49f5833989e8204de45598fe398
lark-gorilla <mark.gr.miller@gmail.com>
2016-04-29 15:39:17 +1000

    update for lord howe data and remove foraging strategy function - this should go in a different script

--- sourced_heron_lhi_compile.R
+++ sourced_heron_lhi_compile.R
@@ -25,6 +25,7 @@ lhi_04<-data.frame(NestID=lhi_04$Nest, DateTime=as.POSIXlt((lhi_04$'date/time')*
 
 lhi_04<-lhi_04[-which(is.na(lhi_04$DateTime)),] #remove NAs
 lhi_04<-lhi_04[lhi_04$NestID!="e2",] #remove egg nest
+lhi_04<-lhi_04[lhi_04$NestID!="13.000000",] # remove ck dead nest
 
 nest_comp<-data.frame(NestID=lhi_04$NestID, DateTime=lhi_04$DateTime, 
                       DateTime_hack=lhi_04$DateTime_hack, AdultID=lhi_04$AdultID, LW=lhi_04$AdultID, RW=lhi_04$AdultID) 
@@ -112,138 +113,21 @@ date_comp[date_comp$LW_corr=="D",]$RW_assn<-unlist(out)[seq(2,length(unlist(out)
 # writing out nest_comp
 write.csv(date_comp, "R_analyses_data/LHI_2014_nest_attendance_cleaned.csv", row.names=F, quote=F)
 
+nest_comp<-read.csv("R_analyses_data/LHI_2014_nest_attendance_cleaned.csv", h=T)
+nest_comp$Date<-nest_comp$Date_hack
+nest_comp$Date<-ymd(nest_comp$Date)
+
+fun_single<-function(x) 
+{ 
+  LWO<-sample(c("B", "A"),1,)
+  if(LWO=="B"){RWO<-"A"}else{RWO<-"B"}
+  return(c(LWO, RWO))}
 
 ### Parameters
 
-D1="2014-02-05"
-D2="2014-02-25" 
+D1="2004-01-31"
+D2="2004-02-28" 
 longallow=FALSE
 feed_fun=fun_single
-
-
-##### !!!!!!!!!!!!!
-
-## bootstrapping
-bs1000_tl<-data.frame(tLength=seq(1:25))
-bs1000_Ntl<-expand.grid(tLength=seq(1:25), NestID=unique(nest_comp$NestID))
-mean_tl<-data.frame(NestID=unique(nest_comp$NestID))
-
-for(h in 1:1000)
-  
-{
-  out<-lapply(nest_comp[nest_comp$LW_corr=="D",]$diff, FUN=feed_fun)  
-  
-  nest_comp[nest_comp$LW_corr=="D",]$LW_assn<-unlist(out)[seq(1,(length(unlist(out))-1),2)]
-  nest_comp[nest_comp$LW_corr=="D",]$RW_assn<-unlist(out)[seq(2,length(unlist(out)),2)]
-  
-  all_trips<-NULL
-  for(i in unique(nest_comp$NestID))
-  {
-    for(j in c("LW", "RW"))
-    {  
-      if(longallow==TRUE)
-      {
-        backs<-NULL
-        if(j=="LW")
-        {
-          backs<-which(nest_comp[nest_comp$NestID==i& nest_comp$Date>=D1 & nest_comp$Date<=D2,]$LW_assn=="B")
-          if(max(backs)<nrow(nest_comp[nest_comp$NestID==i& nest_comp$Date>=D1 & nest_comp$Date<=D2,]))
-          {
-            backs<-c(backs, which(nest_comp[nest_comp$NestID==i& nest_comp$Date>=D1,]$LW_assn=="B")[length(backs)+1])
-            backs<-na.omit(backs)
-          }
-        }
-        
-        if(j=="RW")
-        {
-          backs<-which(nest_comp[nest_comp$NestID==i& nest_comp$Date>=D1 & nest_comp$Date<=D2,]$RW_assn=="B")
-          if(max(backs)<nrow(nest_comp[nest_comp$NestID==i& nest_comp$Date>=D1 & nest_comp$Date<=D2,]))
-          {
-            backs<-c(backs, which(nest_comp[nest_comp$NestID==i& nest_comp$Date>=D1,]$RW_assn=="B")[length(backs)+1])
-            backs<-na.omit(backs)
-          }
-        }
-      }
-      
-      if(longallow==FALSE)
-      {  
-        backs<-NULL
-        if(j=="LW")
-        {backs<-which(nest_comp[nest_comp$NestID==i& nest_comp$Date>=D1 & nest_comp$Date<=D2,]$LW_assn=="B")}
-        if(j=="RW")
-        {backs<-which(nest_comp[nest_comp$NestID==i& nest_comp$Date>=D1 & nest_comp$Date<=D2,]$RW_assn=="B")}
-      }
-      
-      trip_lz<-NULL  
-      for(k in 2:length(backs))
-      {
-        tl1<-backs[k]-backs[k-1]
-        trip_lz<-c(trip_lz, tl1) 
-      }
-      df_internal<-data.frame(NestID=i, BirdID=j, tLength=trip_lz)
-      all_trips<-rbind(all_trips, df_internal)
-    }
-  }
-  #
-  agg1<-aggregate(tLength~NestID, all_trips,  FUN=mean)
-  
-  
-  # to calc hists per individual nests (pairs) %%$$
-  nest_propz<-NULL
-  tripPropN<-expand.grid(tLength=seq(1:25), NestID=unique(nest_comp$NestID), tProp=0)
-  for(i in unique(all_trips$NestID))
-  {
-    if(sum(all_trips[all_trips$NestID==i,]$tLength, na.rm=T)==0){next} # skips nest on rare occasion that both birds are NA
-    df1<-data.frame(NestID=i,table(na.omit(all_trips[all_trips$NestID==i,]$tLength))) # removes NA values for birds in small time selections
-    df1$Var1<-as.numeric(as.character(df1$Var1))
-    df1$TotTrip<-df1$Var1 * df1$Freq 
-    df1$TripProp<-df1$TotTrip/sum(df1$TotTrip)
-    nest_propz<-rbind(nest_propz, df1)
-    
-    for(j in unique(df1$Var1))
-    {
-      tripPropN[tripPropN$NestID==i & tripPropN$tLength==j,]$tProp<-df1[df1$Var1==j,]$TripProp
-    }
-    
-  }
-  # %%$$
-  
-  # to calc hists per individual birds ~~**
-  all_trips$BirdID2<-paste(all_trips$NestID, all_trips$BirdID,sep="_")
-  trip_propz<-NULL
-  for(i in unique(all_trips$BirdID2))
-  {
-    if(is.na(sum(all_trips[all_trips$BirdID2==i,]$tLength))){next} # this next catches any NA birds for whatever reason and removes. corrects warning created in max(backs) above
-    df1<-data.frame(BirdID2=i,table(all_trips[all_trips$BirdID2==i,]$tLength))
-    df1$Var1<-as.numeric(as.character(df1$Var1))
-    df1$TotTrip<-df1$Var1 * df1$Freq 
-    df1$TripProp<-df1$TotTrip/sum(df1$TotTrip)
-    #print(i);print(sum(df1$TotTrip))
-    trip_propz<-rbind(trip_propz, df1)
-  }
-  
-  #length(unique(trip_propz$BirdID2)) #shows how many birds are monitored
-  tripPropB<-NULL
-  for(i in 1:25)
-  { 
-    df1<-data.frame(tLength=i, tProp=sum(trip_propz[trip_propz$Var1==i,]$TripProp)/length(unique(trip_propz$BirdID2)))
-    tripPropB<-rbind(tripPropB, df1)
-  }
-  # ~~**
-  
-  # compile birds bootstrap
-  bs1000_tl<-cbind(bs1000_tl, tripPropB[,2] )
-  names(bs1000_tl)[names(bs1000_tl)=="tripPropB[, 2]"]<-paste("run", h, sep="_")
-  
-  
-  # compile nests bootstrap
-  bs1000_Ntl<-cbind(bs1000_Ntl, tripPropN[,3] )
-  names(bs1000_Ntl)[names(bs1000_Ntl)=="tripPropN[, 3]"]<-paste("run", h, sep="_")
-  
-  #compile mean trips per nest
-  mean_tl<-cbind(mean_tl,agg1[,2])
-  names(mean_tl)[names(mean_tl)=="agg1[, 2]"]<-paste("run", h, sep="_")
-  print(h)
-}
 
 
